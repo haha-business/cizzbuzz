8318469bf81cc7d75ee55395fb93ed784c00ab2a
Artemis Everfree <artemis@artemis.sh>
2024-10-02 17:54:24 -0700

    fizzbuzz-sed.sh

---
+++ weirdo-dimension/fizzbuzz-sed.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# well, it may be the case that this works on the BSDs, but of course it may
+# well not. funny creatures, those BSDs. not as predictable as a herd of GNU
+yes '' | sed '
+	3~3{ s/$/Fizz/ }
+	5~5{ s/$/Buzz/ }
+	t
+	=; d
+'
+
