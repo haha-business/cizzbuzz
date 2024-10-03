7a5ef82d2df4a423e4539a8c160085f16cadc4b8
Artemis Everfree <artemis@artemis.sh>
2024-10-02 19:24:14 -0700

    rebase moment 2

--- weirdo-dimension/fizzbuzz-sed.sh
+++ weirdo-dimension/fizzbuzz-sed.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # well, it may be the case that this works on the BSDs, but of course it may
 # well not. funny creatures, those BSDs. not as predictable as a herd of GNU
-yes '' | sed '
-	3~3{ s/$/Fizz/ }
-	5~5{ s/$/Buzz/ }
+yes '' \
+	| sed '
+		# start with a blank canvas. creatio ex nihilo.
+		3~3{ s/$/Fizz/ }
+		5~5{ s/$/Buzz/ }
 
-	# do you *understand* now? do you feel the weight of enlightenment?
-	t
-	=; d
+		# do you *understand* now? do you feel the weight of enlightenment?
+		t
+		=
+		d
 '
 
