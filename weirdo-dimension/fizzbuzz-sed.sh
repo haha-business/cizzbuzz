9fe7311ff7d64d85b9570e3f92165b76a13f604c
Artemis Everfree <artemis@artemis.sh>
2024-10-02 18:00:45 -0700

    fix line numbers

--- weirdo-dimension/fizzbuzz-sed.sh
+++ weirdo-dimension/fizzbuzz-sed.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # well, it may be the case that this works on the BSDs, but of course it may
 # well not. funny creatures, those BSDs. not as predictable as a herd of GNU
-yes '' | sed '
-	3~3{ s/$/Fizz/ }
-	5~5{ s/$/Buzz/ }
-	t
-	=; d
-'
+yes '' \
+	| sed '
+		3~3{ s/$/Fizz/ }
+		5~5{ s/$/Buzz/ }
+
+		# do you *understand* now? do you feel the weight of enlightenment?
+		t
+		=; d
+	'
 
